0f9e4aa521ff691dfd563f29dfd3599a4381c61c
sallyskst <wmix04@gmail.com>
2017-02-22 11:51:33 -0800

    Add validation for new comment

--- hw4/handlers/editpost_handler.py
+++ hw4/handlers/editpost_handler.py
@@ -6,30 +6,10 @@ from google.appengine.ext import ndb
 class EditPost(BlogHandler):
     @user_owns_post
     def get(self, post):
-        '''
-        if not self.user:
-            self.redirect("/login")
-
-        post = ndb.Key('Post', int(post_id)).get()
-        
-        if not post:
-            self.error(404)
-            return self.render("not_found.html")
-        '''
         self.render("editpost.html", p=post)
 
     @user_owns_post
     def post(self, post):
-        '''
-        if not self.user:
-            return self.redirect("/login")
-
-        post = ndb.Key('Post', int(post_id)).get()
-
-        if not post:
-            self.error(404)
-            return  self.render("not_found.html")    
-        '''
         subject = self.request.get('subject')
         content = self.request.get('content')
         key_id = self.request.get('key_id')
--- hw4/handlers/newcomment_handler.py
+++ hw4/handlers/newcomment_handler.py
@@ -1,20 +1,22 @@
 from handlers.blog_handler import BlogHandler
 from modules.user import User
 from modules.comment import Comment
+from modules.validation import user_owns_post
 
 from google.appengine.ext import ndb
 
 class NewComment(BlogHandler):
-	def get(self, post_id):
-		self.render("newcomment.html", post_id=post_id)
+    @user_owns_post
+    def get(self, post):
+        self.render("newcomment.html", post_id=post.key.id())
 
-	def post(self, post_id):
+    @user_owns_post
+    def post(self, post):
 		comment = self.request.get("comment")
 	
 		if comment:
-			post = ndb.Key('Post', int(post_id)).get()
 			self.user.add_comment(comment, post)
-			self.redirect('/blog/%s' % post_id)	
-		else:
+            self.redirect('/blog/%s' % post.key.id())
+        else:
 			error = "Please add content!"
-			self.render("newcomment.html", comment=comment, post_id=post_id, error=error)	
+            self.render("newcomment.html", comment=comment, post_id=post.key.id(), error=error)
